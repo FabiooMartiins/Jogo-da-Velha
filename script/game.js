f3c54c8acf65bf3c7b9ec5255f558cdf28a41e7a
Fabio Martins <56135005+FabiooMartiins@users.noreply.github.com>
2022-03-27 15:40:33 -0300

    Create game.js

---
+++ script/game.js
@@ -0,0 +1,62 @@
+//iniciar as variáveis
+let board = ['', '', '', '', '', '', '', '', ''];
+let playerTime = 0;
+let symbols = ['O', 'X'];
+let gameOver = false;
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let seq;
+let pos1, pos2, pos3;
+
+function handleMove(position) {
+
+    if (gameOver) {
+        return;
+    }
+
+    if (board[position] == '') {
+        board[position] = symbols[playerTime];
+
+        gameOver = isWin();
+
+        if (gameOver == false) {
+            if (playerTime == 0) {
+                playerTime = 1;
+            } else {
+                playerTime = 0;
+            }
+        }
+    }
+    return gameOver;
+}
+
+function newGame() {
+    document.getElementById(board).value = ['', '', '', '', '', '', '', '', ''];;
+    document.getElementById(playerTime).value = 0;
+    document.getElementById(gameOver).value = false;
+}
+
+function isWin() {
+
+    for (let i = 0; i < winStates.length; i++) {
+        seq = winStates[i];
+
+        pos1 = seq[0];
+        pos2 = seq[1];
+        pos3 = seq[2];
+
+        if ((board[pos1] == board[pos2] && board[pos1] == board[pos3]) && board[pos1] != '') {
+            return true;
+        }
+    }
+
+    return false;
+}
